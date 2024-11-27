8a0416134cfbff36e635937791616157a867b3fb
tomg223 <123344823+tomg223@users.noreply.github.com>
2024-11-27 14:54:35 -0500

    Update ExperienceItem.js

--- app/components/ExperienceItem.js
+++ app/components/ExperienceItem.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import './ExperienceItem.css'; // Make sure the path is correct
-
+import './ExperienceItem.css'; 
 const ExperienceItem = ({ title, description, date, link }) => {
   return (
     <div className="experience-item ">
-      {/* Timeline Dot */}
       <div className="timeline-dot"></div>
-      {/* Vertical Line */}
       <div className="vertical-line"></div>
 
-      {/* Content */}
       <h3 className="text-2xl font-semibold">{title}</h3>
       <p className="text-gray-300 mt-1">{description}</p>
       <p className="text-gray-400 mt-1 text-sm">{date}</p>
